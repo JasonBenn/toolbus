54a78529a7b00b663e75ad7e3dba40c9ff0549f4
Jason Benn <jasoncbenn@gmail.com>
2015-06-18 17:00:27 -0700

    Add Tool ID.

--- app/resources/mission_resource.rb
+++ app/resources/mission_resource.rb
@@ -1,5 +1,5 @@
 class MissionResource < JSONAPI::Resource 
-  attributes :name, :summary, :found_in, :documentation_url, :version_added, :version_removed, :version, :tool_id
+  attributes :name, :summary, :found_in, :documentation_url, :version_added, :version_removed, :version
   
   has_one :tool
   has_many :completions
